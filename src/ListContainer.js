0eea461f2553013fe47aa862bf9f28189648b726
Jesse Akorah <jesse@Jesses-MacBook-Pro.local>
2022-07-16 01:03:12 +0100

    Create list item component

--- src/ListContainer.js
+++ src/ListContainer.js
@@ -1,27 +1,15 @@
 import React from "react";
+import ListItem from "./ListItem";
 
-function ListContainer({ listArr, projects, music }) {
+function ListContainer({ listArr, projects, music, row }) {
   return (
-    <ul className="list-container">
+    <ul className={row ? "list-container row" : "list-container"}>
       {listArr.map((item) => {
         projects && (item.info = item.year + " / " + item.type);
         music &&
           (item.title = item.name) &&
           (item.info = Math.round(item.time / 3600) + " hours");
-        console.log(item);
-        return (
-          <li>
-            <a
-              href={item.link}
-              className="list-item"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <span className="list-info">{item.info}</span>
-              <span className="list-title">{item.title}</span>
-            </a>
-          </li>
-        );
+        return <ListItem item={item} row={row} />;
       })}
     </ul>
   );
---
+++ src/ListItem.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+function ListItem({ item, row }) {
+  const [isHovering, setIsHovering] = useState(false);
+  return (
+    <li>
+      {!row && (
+        <div className={isHovering ? "list-image show" : "list-image"}></div>
+      )}
+
+      <a
+        href={item.link}
+        className="list-item"
+        target="_blank"
+        rel="noopener noreferrer"
+        onMouseEnter={() => setIsHovering(true)}
+        onMouseLeave={() => setIsHovering(false)}
+      >
+        <span className="list-info">{item.info}</span>
+        <span className="list-title">{item.title}</span>
+      </a>
+    </li>
+  );
+}
+
+export default ListItem;
