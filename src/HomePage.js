f5349178ace4ba4999bfd7620c9e2c3e90c25c42
Jesse Akorah <jesse@Jesses-MacBook-Pro.local>
2022-07-16 01:40:56 +0100

    Edit home page text

--- src/HomePage.js
+++ src/HomePage.js
@@ -5,9 +5,11 @@ function HomePage() {
     <div className="home-page">
       <p className="content-text">
         Raised in Lagos, Nigeria, I'm a creative fullstack developer, with keen
-        interest in exploring the artistic possibilies of web development. Thus
-        I am constantly learning and testing the limits of web element
-        manipulation in the pursuit of new artistic expressions.
+        interest in exploring the artistic possibilies of web development. I
+        experiment and use unique designs in the building of products and the
+        creation of immersive web experiences. Thus I am constantly learning and
+        testing the limits of web element manipulation in the pursuit of new
+        artistic expressions.
       </p>
     </div>
   );
