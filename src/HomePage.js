51ccafba4ab5f2f3e1f9028b11e5990c25584691
Jesse Akorah <jesse@Jesses-MacBook-Pro.local>
2022-07-16 01:01:55 +0100

    Edit home page text

--- src/HomePage.js
+++ src/HomePage.js
@@ -4,7 +4,7 @@ function HomePage() {
   return (
     <div className="home-page">
       <p className="content-text">
-        Raised in Lagos, Nigeria, I am a creative fullstack developer, with keen
+        Raised in Lagos, Nigeria, I'm a creative fullstack developer, with keen
         interest in exploring the artistic possibilies of web development. Thus
         I am constantly learning and testing the limits of web element
         manipulation in the pursuit of new artistic expressions.
