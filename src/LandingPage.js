6f23863c32f9c62cc55010543331a2826c37de11
Jesse Akorah <jesseakorah@gmail.com>
2022-11-22 02:00:47 +0100

    Show landing page

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -6,42 +6,44 @@ function LandingPage() {
   const [description, setDescription] = useState(true);
   const [hideLanding, setHideLanding] = useState(false);
 
-  // useEffect(() => {
-  //   const timer = setTimeout(() => {
-  //     setLanding(false);
-  //   }, 1500);
-  //   return () => clearTimeout(timer);
-  // }, []);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLanding(false);
+    }, 1500);
+    return () => clearTimeout(timer);
+  }, []);
 
-  // useEffect(() => {
-  //   const timer = setTimeout(() => {
-  //     setDevName(false);
-  //   }, 1000);
-  //   return () => clearTimeout(timer);
-  // }, []);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDevName(false);
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, []);
 
-  // useEffect(() => {
-  //   const timer = setTimeout(() => {
-  //     setDescription(false);
-  //   }, 1100);
-  //   return () => clearTimeout(timer);
-  // }, []);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDescription(false);
+    }, 1100);
+    return () => clearTimeout(timer);
+  }, []);
 
-  // useEffect(() => {
-  //   const timer = setTimeout(() => {
-  //     setHideLanding(true);
-  //   }, 2300);
-  //   return () => clearTimeout(timer);
-  // }, []);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setHideLanding(true);
+    }, 2300);
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
-    <div className /* {
+    <div
+      className={
         landing && !hideLanding
           ? "landing-page show"
           : !hideLanding
           ? "landing-page"
           : "landing-page none"
-        } */="landing-page none">
+      }
+    >
       <div className="landing-text">
         <h1 className={devName ? "dev-name show" : "dev-name"}>
           Jesse K. Akorah
